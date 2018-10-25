d6324af116b6d4f762f8e43f023be7bb8b65cfb8
Alvin Cheng <ascheng21@gmail.com>
2018-10-24 22:24:29 -0700

    add route that takes in zip as query param

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const redis = require('redis');
 const client = redis.createClient();
 const request = require('request');
@@ -10,72 +11,78 @@ client.on('error', function (err) {
   console.log('Error ' + err);
 });
 
+const app = express()
 
+app.get('/', (req, res) => {
+  const { zip } = req.query
 
-csv()
-.fromStream(request.get('https://s3.amazonaws.com/peerstreet-static/engineering/zip_to_msa/cbsa_to_msa.csv'))
-.subscribe((json)=>{
-  return new Promise((resolve, reject)=>{
-    if (json.LSAD === 'Metropolitan Statistical Area') {
-      client.set('CBSA-' + json.CBSA, JSON.stringify(json));
-
-      if (json.MDIV) {
-        client.set('MDIV-' + json.MDIV, json.CBSA);
-      }
-    }
-    resolve()
-  })
-},
-(err) => console.log('err: ', err),
-() => {
-
-})
-
-csv()
-  .fromStream(request.get('https://s3.amazonaws.com/peerstreet-static/engineering/zip_to_msa/zip_to_cbsa.csv'))
-  .subscribe((json)=>{
-    return new Promise((resolve, reject)=>{
-      client.set('ZIP-' + json.ZIP, json.CBSA);
-      resolve()
-    })
-  },
-  (err) => console.log('err: ', err),
-  () => {
-
-  })
-
-let zip = 32003
-client.get('ZIP-' + zip, function (error, CBSA) {
-  if (error) {
-      console.log(error);
-      throw error;
-  }
-
-  client.get('MDIV-' + CBSA, function (error, newCBSA) {
+  client.get('ZIP-' + zip, function (error, CBSA) {
     if (error) {
         console.log(error);
         throw error;
     }
 
-    const updatedCBSA = newCBSA || CBSA;
-    
-    client.get('CBSA-' + updatedCBSA, function (error, data) {
+    client.get('MDIV-' + CBSA, function (error, newCBSA) {
       if (error) {
           console.log(error);
           throw error;
       }
 
-      data = JSON.parse(data)
-      console.log({
-        Zip: zip,
-        CBSA: CBSA,
-        MSA: data.NAME,
-        Pop2015: data.POPESTIMATE2015,
-        Pop2014: data.POPESTIMATE2014
+      const updatedCBSA = newCBSA || CBSA;
+      
+      client.get('CBSA-' + updatedCBSA, function (error, data) {
+        if (error) {
+            console.log(error);
+            throw error;
+        }
+
+        data = JSON.parse(data)
+        res.json({
+          Zip: zip,
+          CBSA: CBSA,
+          MSA: data.NAME,
+          Pop2015: data.POPESTIMATE2015,
+          Pop2014: data.POPESTIMATE2014
+        })
       });
     });
   });
-});
+})
+
+
+
+
+app.listen(3000, () => {
+  csv()
+    .fromStream(request.get('https://s3.amazonaws.com/peerstreet-static/engineering/zip_to_msa/cbsa_to_msa.csv'))
+    .subscribe((json)=>{
+      return new Promise((resolve, reject)=>{
+        if (json.LSAD === 'Metropolitan Statistical Area') {
+          client.set('CBSA-' + json.CBSA, JSON.stringify(json));
+
+          if (json.MDIV) {
+            client.set('MDIV-' + json.MDIV, json.CBSA);
+          }
+        }
+        resolve()
+      })
+    },
+    (err) => console.log('err: ', err),
+    () => console.log('complete'))
+
+  csv()
+    .fromStream(request.get('https://s3.amazonaws.com/peerstreet-static/engineering/zip_to_msa/zip_to_cbsa.csv'))
+    .subscribe((json)=>{
+      return new Promise((resolve, reject)=>{
+        client.set('ZIP-' + json.ZIP, json.CBSA);
+        resolve()
+      })
+    },
+    (err) => console.log('err: ', err),
+    () => console.log('complete'))
+
+  console.log('Listening on 3000')
+})
 
 
 
